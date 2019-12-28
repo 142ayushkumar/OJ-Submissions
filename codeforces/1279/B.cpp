be6a1a333262cec6edcbd63f2600acd9ac9eb4a9
Kumar Ayush <142ayushkumar@gmail.com>
2019-12-28 08:40:00 +0530

    Add solution for problem `B - Verse For Santa` Link: https://codeforces.com/contest/1279/problem/B Tags: binary search, brute force, implementation, *1300 Ref: https://codeforces.com/contest/1279/submission/67765222

---
+++ codeforces/1279/B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pb push_back
+#define mp make_pair
+#define int long long
+#define all(c) (c).begin(),(c).end()
+#define M 1000000007
+#define INF LLONG_MAX
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ", "; dbs(str.substr(idx + 1), s...);}
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+// Use pr(a,b,c,d,e) or cerr<<anything or prc(v.begin(),v.end()) or prc(v,v+n)
+//  
+
+
+int32_t main()
+{
+    fastio;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, m, tot=0;
+        cin >> n >> m;
+        vector<int> a(n);
+        for(int i=0;i<n;i++) 
+        {
+            cin >> a[i];
+            tot += a[i];
+        }
+        int curr = 0, mx = 0, index = -1, i;
+        for(i=0;i<n;i++)
+        {
+            curr += a[i];
+            if(a[i] > mx) index = i;
+            mx = max(mx, a[i]);
+            if(curr > m) break;
+        }
+        if(tot<=m) cout << "0\n";
+        else if(curr > m && curr - mx < m) cout << index+1 << "\n";
+        else cout << "0\n";
+    }
+
+    return 0;
+}
+
