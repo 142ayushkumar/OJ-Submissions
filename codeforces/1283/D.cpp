2b36acaf3cc04bf2b054bc0db160200f641b66a8
Kumar Ayush <142ayushkumar@gmail.com>
2020-01-21 02:04:00 +0530

    Add solution for problem `D - Christmas Trees` Link: https://codeforces.com/contest/1283/problem/D Tags: graphs, greedy, shortest paths, *1800 Ref: https://codeforces.com/contest/1283/submission/69211926

--- codeforces/1283/D.cpp
+++ codeforces/1283/D.cpp
@@ -18,20 +18,7 @@ template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p)
 template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
 // Use pr(a,b,c,d,e) or cerr<<anything or prc(v.begin(),v.end()) or prc(v,v+n)
 //  
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
 
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
 
 int32_t main()
 {
@@ -41,9 +28,9 @@ int32_t main()
     int n, m;
     cin >> n >> m;
     vector<int> v(n);
-    unordered_map<int, int, custom_hash> mp, tr;
-    //mp.reserve(1024*512);
-    //tr.reserve(1024*512);
+    unordered_map<int, int> mp, tr;
+    mp.reserve(1024*512);
+    tr.reserve(1024*512);
     for(int i=0;i<n;i++)
     {
         cin >> v[i];
