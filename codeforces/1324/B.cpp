712a8f7470c40998f3f297ea3f81a51703c6621c
Kumar Ayush <142ayushkumar@gmail.com>
2020-03-12 18:44:00 +0530

    Add solution for problem `B - Yet Another Palindrome Problem` Link: https://codeforces.com/contest/1324/problem/B Tags: brute force, strings, *1100 Ref: https://codeforces.com/contest/1324/submission/73027961

---
+++ codeforces/1324/B.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI = 3.141592653589;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pb push_back
+#define mp make_pair
+#define int long long
+#define all(c) (c).begin(),(c).end()
+#define M 1000000007
+#define INF LLONG_MAX
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ", "; dbs(str.substr(idx + 1), s...);}
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+// Use pr(a,b,c,d,e) or cerr<<anything or prc(v.begin(),v.end()) or prc(v,v+n)
+//  
+
+
+int32_t main()
+{
+    fastio;
+    //freopen("file.in", "r", stdin);
+    //freopen("file.out", "w", stdout);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        vector<pair<int, int>> c(n+1, {-1, -1});
+        bool ans = false;
+        for(int i=0;i<n;i++)
+        {
+            cin >> v[i];
+            if(c[v[i]].first == -1) c[v[i]].first = i;
+            else if(c[v[i]].second != -1)
+            {
+                ans = true;
+            }
+            else 
+            {
+                c[v[i]].second = i;
+                if(c[v[i]].second - c[v[i]].first > 1)
+                    ans = true;
+            }
+        }
+        if(ans) cout << "YES\n";
+        else cout << "NO\n";
+
+                    
+
+
+    }
+    return 0;
+}
+
